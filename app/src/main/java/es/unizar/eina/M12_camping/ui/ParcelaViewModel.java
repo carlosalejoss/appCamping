a4b3919e573db6f9a677585ccfa32edc5210326f
carlosalejoss <872342@unizar.es>
2024-11-11 19:25:40 +0100

    añade métodos en ParcelaViewModel para obtener parcelas ordenadas por nombre, ocupantes y precio

--- app/src/main/java/es/unizar/eina/M12_camping/ui/ParcelaViewModel.java
+++ app/src/main/java/es/unizar/eina/M12_camping/ui/ParcelaViewModel.java
@@ -15,14 +15,32 @@ public class ParcelaViewModel extends AndroidViewModel {
     private ParcelaRepository mRepository;
 
     private final LiveData<List<Parcela>> mAllParcelas;
+    private final LiveData<List<Parcela>> mParcelasOrdNombre;
+    private final LiveData<List<Parcela>> mParcelasOrdOcupantes;
+    private final LiveData<List<Parcela>> mParcelasOrdPrecio;
 
     public ParcelaViewModel(Application application) {
         super(application);
         mRepository = new ParcelaRepository(application);
         mAllParcelas = mRepository.getAllParcelas();
+        mParcelasOrdNombre = mRepository.getParcelasOrderedNombre();
+        mParcelasOrdOcupantes = mRepository.getParcelasOrderedOcupantes();
+        mParcelasOrdPrecio = mRepository.getParcelasOrderedPrecio();
     }
 
     LiveData<List<Parcela>> getAllParcelas() { return mAllParcelas; }
+
+    LiveData<List<Parcela>> getParcelasOrderedNombre() {
+        return mParcelasOrdNombre;
+    }
+
+    LiveData<List<Parcela>> getParcelasOrderedOcupantes() {
+        return mParcelasOrdOcupantes;
+    }
+
+    LiveData<List<Parcela>> getParcelasOrderedPrecio() {
+        return mParcelasOrdPrecio;
+    }
 
     public void insert(Parcela parcela) { mRepository.insert(parcela); }
 
