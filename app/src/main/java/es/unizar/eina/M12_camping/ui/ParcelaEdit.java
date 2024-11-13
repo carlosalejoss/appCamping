3de6718a48450652f6f9c78f6c59f98530fb3292
carlosalejoss <872342@unizar.es>
2024-11-13 16:32:36 +0100

    cambia el layout de ParcelaEdit para utilizar el nuevo diseño de actividad

--- app/src/main/java/es/unizar/eina/M12_camping/ui/ParcelaEdit.java
+++ app/src/main/java/es/unizar/eina/M12_camping/ui/ParcelaEdit.java
@@ -44,7 +44,7 @@ public class ParcelaEdit extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_noteedit);
+        setContentView(R.layout.activity_parcelaedit);
 
         mNombreText = findViewById(R.id.nombre);
         mMaxOcupantes = findViewById(R.id.maxOcupantes);
