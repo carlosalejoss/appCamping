e2371c24c4def2e3027344eaadb0073d62a9df8d
carlosalejoss <872342@unizar.es>
2024-11-15 15:51:16 +0100

    Actualiza el nombre del paquete en el test instrumentado a ListadoParcelas

--- app/src/androidTest/java/es/unizar/eina/M12_camping/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/es/unizar/eina/M12_camping/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("es.unizar.eina.M12_camping", appContext.getPackageName());
+        assertEquals("es.unizar.eina.ListadoParcelas", appContext.getPackageName());
     }
 }
